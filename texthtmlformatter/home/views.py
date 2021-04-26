3e879d422de2299948140e0873a336116bb1cd5f
Hubcodee <anshshrivas26@gmail.com>
2021-04-26 12:58:23 +0530

    content addition and template change - @ansh

--- texthtmlformatter/home/views.py
+++ texthtmlformatter/home/views.py
@@ -1,18 +1,24 @@
-from django.shortcuts import render,HttpResponse
+from django.shortcuts import render, HttpResponse
 from .texttohtml import formatHtml
+from django.template.loader import render_to_string
+
 
 def index(request):
-    return(render(request,"index.html"))
+    return(render(request, "index.html"))
+
 
 def inputfile(request):
-    return(render(request,"input.html"))
+    return(render(request, "input.html"))
+
 
 def result(request):
-    if request.method=="POST":
-        text=request.POST.get("textcode")
-        htmlcode=formatHtml("\n\n"+text+"\n\n")   # html generated code is coming from this function
+    if request.method == "POST":
+        text = request.POST.get("textcode")
+        # html generated code is coming from this function
+        htmlcode = formatHtml("\n\n"+text+"\n\n")
         print(htmlcode)
-        return(render(request,"result.html",{"htmlcode":str(htmlcode)}))
+
+        return(render(request, "result.html", {"htmlcode": str(htmlcode)}))
     else:
-        htmlcode="<h2>Oops there seems some problem, Please go back and resumit your Text</h2>"
+        htmlcode = "<h2>Oops there seems some problem, Please go back and resumit your Text</h2>"
         return(HttpResponse(htmlcode))
